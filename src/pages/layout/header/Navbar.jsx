a999d18b831e1a3ea0cf7a740a35fdecd8564af9
Sombhu2022 <sombhudas93@gmail.com>
2024-07-21 00:56:07 +0530

    home page

--- src/pages/layout/header/Navbar.jsx
+++ src/pages/layout/header/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import dp from '../../../assets/profile.jpg'
 import { CgDetailsMore } from "react-icons/cg";
+import { MdOutlineClose } from "react-icons/md";
 import { useState } from "react";
 const Navbar = () => {
  
@@ -12,14 +13,17 @@ const Navbar = () => {
 
   return (
     
-    <header className=" p-3 w-[100vw] flex justify-between lg:pl-20 lg:pr-20 md:pl-3 md:pr-3  bg-[#083b3b0f] border  border-[#8090c038] ">
+    <header className="p-3 w-full flex justify-between lg:pl-20 lg:pr-20 md:pl-3 md:pr-3  bg-[#083b3b0f] border  border-[#8090c038] ">
       <div className="text-3xl font-serif"> DOCTOMEET </div>
       <nav className="flex items-center justify-center gap-2">
       <NavLink to={"/doctors"} className={"border rounded-[5px] p-3 pt-1 pb-1 border-[#8090c038]"}>
       Login 
       </NavLink>
       <Link className="overflow-hidden"> <img className="h-7 w-7 border rounded-[50%] " src={dp} alt="" /> </Link>
-      <CgDetailsMore className="size-7 cursor-pointer" onClick={toggleMenu} />
+      
+      {
+        menuOpen ? <MdOutlineClose className="size-7 cursor-pointer" onClick={toggleMenu}  />:<CgDetailsMore className="size-7 cursor-pointer" onClick={toggleMenu} />
+      }
       </nav>
       {menuOpen && (
         <div className="absolute top-16 right-3 w-40  shadow-lg flex gap-2 flex-col">
