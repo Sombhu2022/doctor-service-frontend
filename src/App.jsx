8dba2b5659f89d278c014e34eb8c56be784aa22e
Sombhu2022 <sombhudas93@gmail.com>
2024-11-13 02:00:48 +0530

    aws add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import Layout from './pages/layout/Layout'
 import Home from './pages/home/Home'
+import Doctor from './pages/doctor/Doctor'
+import UploadFile from './pages/doctor/UploadFile'
 
 function App() {
 
@@ -12,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
+        {/* <Route path='/doctors' element={<Doctor/>}/> */}
+        <Route path='/doctors' element={<UploadFile/>}/>
+
         </Route>
       </Routes>
      </Router>
@@ -20,3 +25,5 @@ function App() {
 }
 
 export default App
+
+export const baseUrl = 'http://localhost:8080/api/v1'
